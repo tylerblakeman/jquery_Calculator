6b14f05a58e534c29117bc2742c8225728483b6e
Firecraker <mezamic000@gmail.com>
2020-03-16 20:44:50 -0500

    added outline for if else statement to be used for operators

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,31 @@
 $(document).ready(function() {
 
-var firstNumber = ""
+var firstNumber = "" 
 var secondNumber = ""
 var operator = ""
 var totalResult = ""
+var operatorArray = ["plus", "minus", "times", "divide", "power"]
 
 
 $("button").on("click", function(){
+var buttonClick = this.getAttribute("value");
+if (operatorArray.includes(buttonClick)){
 
+}
+else if (buttonClick === "clear" || buttonClick === "equals"){
+
+}
+else {
+    if (operator) {
+    
+    } 
+else {
+
+firstNumber += buttonClick
+console.log(firstNumber)
+}
+}
+}
 })
 
 
