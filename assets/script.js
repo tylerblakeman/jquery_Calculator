b6b9a19e187a9237ce46e0d97ea1e6c7888ca0cf
Jacob Zirbel <jzerble@gmail.com>
2020-03-16 20:57:00 -0500

    display number or operator

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,52 @@
 $(document).ready(function() {
+	var firstNumber = "";
+	var secondNumber = "";
+	var operator = "";
+	var totalResult = "";
+	var operatorArray = ["plus", "minus", "times", "divide", "power"];
+	var firstNumDisplay = $("#first-number");
+	var secondNumDisplay = $("#second-number");
+	var operatorDisplay = $("#operator");
+	var resultDisplay = $("#result");
 
-var firstNumber = "" 
-var secondNumber = ""
-var operator = ""
-var totalResult = ""
-var operatorArray = ["plus", "minus", "times", "divide", "power"]
-
-
-$("button").on("click", function(){
-var buttonClick = this.getAttribute("value");
-if (operatorArray.includes(buttonClick)){
-
-}
-else if (buttonClick === "clear" || buttonClick === "equals"){
-
-}
-else {
-    if (operator) {
-    
-    } 
-else {
-
-firstNumber += buttonClick
-console.log(firstNumber)
-}
-}
-}
-})
-
-
-
-
+	$("button").on("click", function() {
+		var buttonClick = this.getAttribute("value");
+		if (operatorArray.includes(buttonClick)) {
+			if (firstNumber) {
+				operator = buttonClick;
+				switch (operator) {
+					case "minus":
+						operator = "-";
+						break;
+					case "plus":
+						operator = "+";
+						break;
+					case "times":
+						operator = "x";
+						break;
+					case "divide":
+						operator = "÷";
+						break;
+					case "power":
+						operator = "^";
+						break;
+					default:
+						break;
+				}
+				operatorDisplay.text(operator);
+			}
+		} else if (buttonClick === "clear") {
+		} else if (buttonClick === "equals") {
+		} else {
+			if (operator) {
+				secondNumber += buttonClick;
+				console.log(secondNumber);
+				secondNumDisplay.text(secondNumber);
+			} else {
+				firstNumber += buttonClick;
+				console.log(firstNumber);
+				firstNumDisplay.text(firstNumber);
+			}
+		}
+	});
 });
-
